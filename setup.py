e9bd19c7e4a1ed14f0afc0b3b1f673139044d573
carlino.calogero <carlino.calogero@C0013636.local>
2024-11-19 11:23:27 +0100

    added setup.py

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+setup(name = 'spacenumbat',
+     version = '0.0.1',
+     description = 'Haplotype aware CNAs caller with a spatial context',
+     url = 'https://github.com/lillux/spacenumbat',
+     author = 'Calogero Carlino',
+     author_email = 'calogero.carlino28@gmail.com',
+     license = 'GPLv3',
+     zip_safe=False,
+     install_requires=['numpy',
+                       'matplotlib',
+                       'scipy',
+                       'anndata',
+                       'pyranges',
+                       'joblib'],
+                       
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'Intended Audience :: Education',
+          'Intended Audience :: Science/Research',
+          'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+          'Programming Language :: Python :: 3.12',
+          'Topic :: Software Development :: Libraries',
+          'Topic :: Software Development :: Libraries :: Python Modules',
+          'Topic :: Scientific/Engineering :: Bio-Informatics'
+      ],
+              
+      packages=find_packages())
