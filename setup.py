9a323000a59d5e6d612f0a96bd350cad7953a594
Carlino <lillux94@gmail.com>
2026-04-10 20:43:28 -0500

    bump to 0.1.0 for realease

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name = 'spacenumbat',
-     version = '0.0.1',
+     version = '0.1.0',
      description = 'Haplotype aware CNAs caller with a spatial context',
      url = 'https://github.com/lillux/spacenumbat',
      author = 'Calogero Carlino, Valentina Giansanti, Davide Cittaro',
@@ -21,7 +21,7 @@ setup(name = 'spacenumbat',
           'Intended Audience :: Education',
           'Intended Audience :: Science/Research',
           'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
-          'Programming Language :: Python :: 3.12',
+          'Programming Language :: Python :: 3.13',
           'Topic :: Software Development :: Libraries',
           'Topic :: Software Development :: Libraries :: Python Modules',
           'Topic :: Scientific/Engineering :: Bio-Informatics'
