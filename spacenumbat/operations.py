c4ba50db39753242d8a0e649ca7e58903ab1ac44
Carlino <lillux94@gmail.com>
2026-05-28 18:59:19 -0500

    remove warning at get_joint_post compute_posterior call after setting nan to 0

--- spacenumbat/operations.py
+++ spacenumbat/operations.py
@@ -1841,15 +1841,15 @@ def get_joint_post(
     joint_post_sp = pd.merge(exp_sel, allele_sel, on=['cell', 'CHROM', 'seg', 'cnv_state'], how='outer')
 
     # Replace NA values in all columns ending with _x or _y with 0. These are {'l*', 'Z*', 'logBF' }
-    # for col in joint_post_sp.columns:
-    #   if col.endswith('_x') or col.endswith('_y'):
-    #       joint_post_sp[col] = joint_post_sp[col].fillna(0)
+    for col in joint_post_sp.columns:
+      if col.endswith('_x') or col.endswith('_y'):
+          joint_post_sp[col] = joint_post_sp[col].fillna(0)
     
     # Only fill additive log-likelihood contributions  # TODO: new
-    lik_cols = [f"{c}_{sfx}" for sfx in ("x", "y") for c in ("l11","l20","l10","l21","l31","l22","l00")]
-    for c in lik_cols:
-        if c in joint_post_sp.columns:
-            joint_post_sp[c] = joint_post_sp[c].fillna(0.0)
+    #lik_cols = [f"{c}_{sfx}" for sfx in ("x", "y") for c in ("l11","l20","l10","l21","l31","l22","l00")]
+    #for c in lik_cols:
+    #    if c in joint_post_sp.columns:
+    #        joint_post_sp[c] = joint_post_sp[c].fillna(0.0)
     
     # Left join with segs_consensus
     segs_sel = segs_consensus.loc[:,
@@ -1880,9 +1880,9 @@ def get_joint_post(
        joint_post_sp[col] = joint_post_sp[f'{col}_x'] + joint_post_sp[f'{col}_y']
     
     # Compute the joint posterior # TODO: fix warnings
-    warnings.filterwarnings('ignore')
+    #warnings.filterwarnings('ignore')
     joint_post_sp = compute_posterior(joint_post_sp)
-    warnings.filterwarnings('always')
+    #warnings.filterwarnings('always')
     
     # Compute logistic transformations for logBF values.
     joint_post_sp['p_cnv_x'] = 1 / (1 + np.exp(-joint_post_sp['logBF_x']))
