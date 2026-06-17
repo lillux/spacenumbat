Fixed scipy.sparse.diags importin hmrf. Revrite for cna_postprocessing to remove local posterior computation

spacenumbat/hmrf.py
@@ -109,10 +109,10 @@ def _mean_field_potts(
                          f"{adjacency.shape} and {log_scores.shape}.")
 
     D = np.asarray(A.sum(0)).ravel()
-    D_inv_sqrt = sp.diag(np.divide(1,
-                                   np.sqrt(D),
-                                   where=D>0, 
-                                   out=np.zeros_like(D)))
+    D_inv_sqrt = sp.diags(np.divide(1,
+                                    np.sqrt(D),
+                                    where=D>0, 
+                                    out=np.zeros_like(D)))
     A_norm = D_inv_sqrt @ A @ D_inv_sqrt
     
     probabilities = _row_softmax(log_scores)